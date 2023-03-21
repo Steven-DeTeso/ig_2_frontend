23429e42c26077f1bfb1264344836d7950947dd1
Steven DeTeso <sadeteso@gmail.com>
2023-03-21 17:35:12 -0400

    refactoring of login/reg components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,51 +1,74 @@
-import React, { useContext, useState } from 'react';
-import axios from 'axios';
-import AuthContext from './AuthContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import AuthContext from "./AuthContext";
+import { useNavigate } from "react-router-dom";
 
-export default function Login () {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+export default function Login() {
+  const { setToken } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const { setToken } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handelSubmit = async (event) => {
-        event.preventDefault();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-        try {
-            const response = await axios.post('http://localhost:8000/api/token/', {
-                email: email,
-                password: password,
-            });
-            console.log(response.data)
-            const token = response.data.access;
-            setToken(token);
-            localStorage.setItem('token', token);
-            navigate('/feed')
-        } catch (error) {
-            console.error('Error: ', error);
-        }
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handelSubmit}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-                <input
-                    type="password" 
-                    placeholder='Password'
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-                <button type='submit'>Login</button>
-            </form>
-        </div>
-    )
-};
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/token/",
+        formValues
+      );
+      const token = response.data.access;
+      setToken(token);
+      localStorage.setItem("token", token);
+      navigate("/feed");
+    } catch (error) {
+      console.log("Error: ", error);
+      setErrorMessage("Invalid email or password.");
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      {errorMessage && <p>{errorMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formValues.email}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formValues.password}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Loading..." : "Login"}
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,86 +1,70 @@
-import React, { useContext, useState } from 'react';
-import axios from 'axios';
-import AuthContext from './AuthContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import AuthContext from "./AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+  const { setToken } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const { setToken } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    username: "",
+    firstName: "",
+    lastName: "",
+  });
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
 
     try {
-        const response = await axios.post('http://localhost:8000/users/', {
-            email: email,
-            password: password,
-            username: username,
-            first_name: firstName,
-            last_name: lastName,
-        });
-
-        console.log(response.data);
-        const token = response.data.access;
-        setToken(token);
-        localStorage.setItem('token', token);
-        navigate('/feed')
+      const response = await axios.post(
+        "http://localhost:8000/users/",
+        formData
+      );
+      console.log(response.data);
+      const token = response.data.access;
+      setToken(token);
+      localStorage.setItem("token", token);
+      navigate("/feed");
     } catch (error) {
-        console.error('Error:', error);
+      console.error("Error:", error);
+      setErrorMessage("Invalid email or password.");
     }
-};
+    setIsLoading(false);
+  };
 
-    return (
-        <div>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='username'>Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                />
-                <label htmlFor='email'>Email</label>
-                <input
-                    id='email'
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-                <label htmlFor='password'>Password</label>
-                <input
-                    id='password'
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-                <label htmlFor='firstName'>First Name</label>
-                <input
-                    id='firstName'
-                    type="text"
-                    placeholder='First Name'
-                    value={firstName}
-                    onChange={(event) => setFirstName(event.target.value)}
-                />
-                <label htmlFor='lastName'>Last Name</label>
-                <input
-                    id='lastName'
-                    type="text"
-                    placeholder='Last Name'
-                    value={lastName}
-                    onChange={(event) => setLastName(event.target.value)}
-                />
-                <button type="submit">Register</button>
-            </form>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>Register</h1>
+      {errorMessage && <p>{errorMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        {Object.keys(formData).map((key) => (
+          <div key={key}>
+            <label htmlFor={key}>{key}</label>
+            <input
+              id={key}
+              type={key === "password" ? "password" : "text"}
+              value={formData[key]}
+              onChange={handleChange}
+            />
+          </div>
+        ))}
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Loading..." : "Register"}
+        </button>
+      </form>
+    </div>
+  );
+}
