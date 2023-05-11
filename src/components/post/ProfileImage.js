160b456e1ecec69a18ba552d0b2f25f4e325f0c2
Steven DeTeso <111303443+Steven-DeTeso@users.noreply.github.com>
2023-05-11 13:06:21 -0400

    missed a few empty lines, also, this PR will have options button rework and suggestrd post rework in it

--- src/components/post/ProfileImage.js
+++ src/components/post/ProfileImage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Avatar from "@mui/material/Avatar";
 import styles from "./ProfileImage.module.css";
 
 export default function ProfileImage({ imageUrl }) {
@@ -14,12 +13,6 @@ export default function ProfileImage({ imageUrl }) {
       ) : (
         <p>upload a profile image</p>
       )}
-      {/* <Avatar
-        src={imageUrl}
-        variant="circular"
-        alt="profile photo"
-        sx={{ width: 50, height: 50 }}
-      /> */}
     </>
   );
 }
